a8e177a481b73d0f037440926562658ed1796b44
jon27 <jon.cabre@gmail.com>
2018-08-03 14:05:20 -0400

    fixed links to projects

--- src/components/ShowcaseProjects.js
+++ src/components/ShowcaseProjects.js
@@ -7,41 +7,41 @@ const ShowcaseProjects = () => (
 		</div>
 		<div>
 			<div className='project__row'>
-				<a href='#' className='project__item exp project__box '>
+				<a href='https://expensify-app-jon.herokuapp.com/' target="_blank" className='project__item exp project__box '>
 					<div>
 						<p className='project-overlay'>Expensify</p>
 					</div>
 				</a>
-				<a href='#' className='project__item tic project__box '>
+				<a href='http://react-weather-app-jon.herokuapp.com/#/?_k=3qtqvl' target="_blank" className='project__item tic project__box '>
 					<div>
 						<p className='project-overlay'>Weather App</p>
 					</div>
 				</a>
-				<a href='#' className='project__item troll project__box '>
+				<a href='https://whack-a-troll.herokuapp.com/' target="_blank" className='project__item troll project__box '>
 					<div>
 						<p className='project-overlay'>Wack A Troll</p>
 					</div>
 				</a>				
 			</div>
 			<div className='project__row'>
-				<a href='#' className='project__item drum project__box '>
+				<a href='https://drop-the-beat.herokuapp.com/' target="_blank" className='project__item drum project__box '>
 					<div>
 						<p className='project-overlay'>Drop the Beat</p>
 					</div>
 				</a>
-				<a href='#' className='project__item omni project__box '>
+				<a href='https://omnifood-.herokuapp.com/' target="_blank" className='project__item omni project__box '>
 					<div>
 						<p className='project-overlay'>OmniFood</p>
 					</div>
 				</a>
-				<a href='#' className='project__item canvas project__box '>
+				<a href='https://picaso.herokuapp.com/' target="_blank" className='project__item canvas project__box '>
 					<div>
 						<p className='project-overlay'>Canvas Drawing</p>
 					</div>
 				</a>				
 			</div>
 			<div className='other-projects-box'>
-				<a className='other-projects' href='#'>Other Projects</a>
+				<a className='other-projects' href='#' target="_blank">Other Projects</a>
 			</div>
 		</div>
 	</div>
